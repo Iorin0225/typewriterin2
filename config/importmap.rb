da1a9cfc0e5cef03733fa56fd2e80b293cd1a6ea
Iori OSADA <iori.osada@gmail.com>
2024-11-02 09:22:34 +0900

    feat: add font-awesome

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,3 +5,7 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
+
+# Custom
+# Don't forget to write "import" on js
+pin "font-awesome", to: "https://kit.fontawesome.com/8ddaa3dfab.js", preload: true
